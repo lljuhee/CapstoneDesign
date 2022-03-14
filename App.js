606151614fa26bbbabf51f86352375f1f2755822
lljuhee <lljuhee2@naver.com>
2022-03-14 18:36:29 +0900

    CapstoneDesign

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Capstone Design - 2조 </Text>
+      <Text>DKU 스터디룸 예약 시스템 </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
